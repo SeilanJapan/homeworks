1e6f734e54300e95a74686ff0d5daeec3e70494d
Petya Ilieva <pipion@abv.bg>
2015-07-12 08:53:21 +0300

    Create 1zadA.php

---
+++ Hmework7/1zad/1zadA.php
@@ -0,0 +1,21 @@
+<! DOCTYPE html>
+<html>
+		<head>
+			<head>
+
+				<!-- Latest compiled and minified CSS -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+				<!-- Latest compiled and minified JavaScript -->
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		</head> 
+		<body>
+			<img src="dog.jpg" alt="dog" class="img-circle img-responsive">
+			<img src="cat.jpg" alt="cat" class="img-circle img-responsive">
+		</body>
+
+</html>
