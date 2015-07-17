eb486061d487f27e285609dc12799fdaeea30e5b
Petya Ilieva <pipion@abv.bg>
2015-07-17 20:18:32 +0300

    Create 1zadB.php

---
+++ Hmework7/1zad/1zadB.php
@@ -0,0 +1,69 @@
+<! DOCTYPE html>
+<html>
+		<head>
+			<head>
+
+				<!-- Latest compiled and minified CSS -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+				<!-- Latest compiled and minified JavaScript -->
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		</head> 
+
+		<body>
+
+			<?php
+		
+
+			?>
+		
+			<div class="row">
+  				<div class="col-sm-6 col-md-4">
+   					 <div class="thumbnail">
+     					 <img src="http://www.sputnikmusic.com/blog/wp-content/uploads/CATS.jpg" class="img-responsive" alt="Responsive image">
+     						 <div class="caption">
+       							 <h3>Education!</h3>
+       								 <p>Free classes for school!</p>
+       								 <p>
+       								 	<a href="#" class="btn btn-primary" role="button">Yes</a>
+       								 	<a href="#" class="btn btn-default" role="button">No</a>
+       								 </p>
+      						</div>
+    				</div>
+
+  				</div>
+  				<div class="col-sm-6 col-md-4">
+   					 <div class="thumbnail">
+     					 <img src="http://s3.amazonaws.com/photos.ecarlist.com/staff/14/21/07/99/97/56/32/51/14/Karlee%20Cats.jpg" class="img-responsive" alt="Responsive image">
+     						 <div class="caption">
+       							 <h3>Welcome to our school!</h3>
+       								 <p>If you want to learn something new join to us! It`s free!</p>
+       								 <p>
+       								 	<a href="#" class="btn btn-primary" role="button">Yes</a>
+       								 	<a href="#" class="btn btn-default" role="button">No</a>
+       								 </p>
+      						</div>
+    				</div>
+  				</div>
+  				<div class="col-sm-6 col-md-4">
+   					 <div class="thumbnail">
+     					 <img src="http://s3-media4.fl.yelpcdn.com/bphoto/cwS9-pIKsOdrxa9U0HIhFg/ls.jpg" class="img-responsive" alt="Responsive image">
+     						 <div class="caption">
+       							 <h3>Be volunteer!</h3>
+       								 <p>Join to us and make good!</p>
+       								 <p>
+       								 	<a href="#" class="btn btn-primary" role="button">Yes</a>
+       								 	<a href="#" class="btn btn-default" role="button">No</a>
+       								 </p>
+      						</div>
+    				</div>
+    				
+  				</div>
+			</div>
+		</body>
+
+	</html>
