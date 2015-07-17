f82dc738d1e1d7e8ff91fbaac2b19356757c2bd0
Petya Ilieva <pipion@abv.bg>
2015-07-17 20:36:49 +0300

    Create 2zad.php

---
+++ Hmework7/2zad.php
@@ -0,0 +1,44 @@
+<! DOCTYPE html>
+<html>
+		<head>
+			<head>
+
+				<!-- Latest compiled and minified CSS -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+				<!-- Latest compiled and minified JavaScript -->
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		</head> 
+		<body>
+
+			<div class="list-group">
+  				
+  				<a href="http://www.telegraph.co.uk/film/ant-man/" class="list-group-item active">
+   					 <h4 class="list-group-item-heading">ANT-MAN</h4>
+   					 <p class="list-group-item-text">SYNOPSIS: Former petty criminal Scott Lang must become the next Ant-Man, donning a suit that allows him to shrink in size while maintaining his strength, and pull off a heist that will save the world.</p>
+ 				 </a>
+ 			
+ 				 <a href="http://www.telegraph.co.uk/film/the-wonders/" class="list-group-item active">
+				    <h4 class="list-group-item-heading">THE WONDERS</h4>
+				    <p class="list-group-item-text">SYNOPSIS: A rural family's life is disrupted by the arrival of a reality TV crew</p>
+  				</a>
+  				
+  				<a href="http://www.telegraph.co.uk/film/selfless/" class="list-group-item active">
+				    <h4 class="list-group-item-heading">SELF/LESS</h4>
+				    <p class="list-group-item-text">SYNOPSIS: A dying billionaire transfers his consciousness into another man's body</p>
+  				</a>
+  				
+  				<a href="http://www.telegraph.co.uk/film/salt-of-the-earth/" class="list-group-item active">
+				    <h4 class="list-group-item-heading">THE SALT OF THE EARTH</h4>
+				    <p class="list-group-item-text">SYNOPSIS: Documentary about the life and work of photographer Sebastião Salgado</p>
+  				
+  				</a>
+			</div>
+		
+		</body>
+
+</html>
