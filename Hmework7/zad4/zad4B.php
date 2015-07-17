88123dfb8575f541b5aaf8591cb8bef04b261423
Petya Ilieva <pipion@abv.bg>
2015-07-17 23:13:22 +0300

    Create zad4B.php

---
+++ Hmework7/zad4/zad4B.php
@@ -0,0 +1,139 @@
+<! DOCTYPE html>
+<html>
+		<head>
+			<head>
+
+				<!-- Latest compiled and minified CSS -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+				<!-- Latest compiled and minified JavaScript -->
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		</head> 
+		<body>
+			
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:6%;"><h3 style="color:pink; font-size:100px; text-align:center;">0</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">a</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">b</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">c</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">d</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">e</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">f</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">g</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">h</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:5%; height:6%;"><h3 style="color:pink; font-size:100px; text-align:center;">0</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">8</h3></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			    <div class="col-md-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">8</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">7</h3></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">7</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">6</h3></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">6</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">5</h3></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">5</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">4</h3></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">4</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">3</h3></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">3</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">2</h3></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">2</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">1</h3></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">a</span></div>
+			  <div class="col-md-1" style="border:1px; background:black; width:10%; height:18%;"><span style="color:black;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:blue; width:10%; height:18%;"><span style="color:blue;">x</span></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:5%; height:18%;"><h3 style="color:black; font-size:100px; text-align:center;">1</h3></div>
+			</div>
+			<div class="row">
+				<div class="col-md-1 col-md-offset-1" style="border:1px; background:pink; width:5%; height:6%;"><h3 style="color:pink; font-size:100px; text-align:center;">0</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">a</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">b</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">c</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">d</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">e</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">f</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">g</h3></div>
+			  <div class="col-md-1" style="border:1px; background:pink; width:10%; height:6%;"><h3 style="color:black; font-size:100px; text-align:center;">h</h3></div>
+			     <div class="col-md-1" style="border:1px; background:pink; width:5%; height:6%;"><h3 style="color:pink; font-size:100px; text-align:center;">0</h3></div>
+			</div>
+
+		</body>
